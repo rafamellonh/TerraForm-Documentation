eaac6f95eff6272aad664f175ce74a10e7936a2a
rafael Mello <rmellonh@gmail.com>
2024-10-27 13:37:36 -0400

    Terraform

--- 2024/provider.tf
+++
@@ -1,5 +0,0 @@
-
-
-
-
-#az role assignment create --assignee e3831ec2-1f30-400b-870c-fa682f08771e --role Contributor --scope /subscriptions/b0586674-03cf-4c83-a967-e5d72c607b1a/resourceGroups/Rafael
