dcfac6973ef6965abc5f09c121a4b8f12c4a0aa4
rafael Mello <rmellonh@gmail.com>
2024-10-27 13:31:42 -0400

    Terraform

---
+++ 2024/provider.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+    features {
+    }
+    subscription_id = "b0586674-03cf-4c83-a967-e5d72c607b1a"
+    client_id = "e3831ec2-1f30-400b-870c-fa682f08771e"
+    client_secret = "AeZ8Q~z-VG8PAv38sBcMmCpJ-NCcmSAmVVGdZaBg"
+    tenant_id = "31afaf0d-4d06-4349-b0a9-83d163512c8a"
+}
+
+
+
+
+#az role assignment create --assignee e3831ec2-1f30-400b-870c-fa682f08771e --role Contributor --scope /subscriptions/b0586674-03cf-4c83-a967-e5d72c607b1a/resourceGroups/Rafael
