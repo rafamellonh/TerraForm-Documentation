041ba1b4348993ca7b59b12d534edbd77cf9ca95
rafael Mello <rmellonh@gmail.com>
2024-10-27 13:36:47 -0400

    Terraform

--- 2024/provider.tf
+++ 2024/provider.tf
@@ -1,7 +1,7 @@
 #provider "azurerm" {
-    features {
-    }
-    subscription_id = "b0586674-03cf-4c83-a967-e5d72c607b1a"
+#    features {
+ #   }
+  #  subscription_id = "b0586674-03cf-4c83-a967-e5d72c607b1a"
     client_id = "e3831ec2-1f30-400b-870c-fa682f08771e"
     client_secret = "AeZ8Q~z-VG8PAv38sBcMmCpJ-NCcmSAmVVGdZaBg"
     tenant_id = "31afaf0d-4d06-4349-b0a9-83d163512c8a"
