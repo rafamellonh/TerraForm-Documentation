a6e6a4da0d990213f14740ab16bbd47fbafd26bd
rafael Mello <rmellonh@gmail.com>
2024-10-27 22:01:38 -0400

    Terraform

---
+++ 2024/MOD01/resource-group.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "Rafael" {
+    location = "eastus"
+    name = "Rafael"    
+}
+
+#  terraform import azurerm_resource_group.Rafael /subscriptions/Sb0586674-03cf-4c83-a967-e5d72c607b1a/resourceGroups/Rafael
