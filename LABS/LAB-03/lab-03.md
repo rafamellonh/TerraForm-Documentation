784c1f1671375f66dec1c7c1622725d1c7b7c2ef
rafael Mello <rmellonh@gmail.com>
2025-03-24 22:13:48 -0400

    Terraform

--- LABS/LAB-03/lab-03.md
+++ LABS/LAB-03/lab-03.md
@@ -1,2 +1,6 @@
 This lab includes :
 Terraform modules
+Create module DNS with the variables, output file.
+Import the module dns in the main.tf
+configure the vars importing  module
+Use data. for use  ressource group
--- LABS/LAB-03/main.tf
+++ LABS/LAB-03/main.tf
@@ -1,31 +1,31 @@
 terraform {
-    required_providers {
-        azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "4.23.0"
-        }
-    
-        random = {
-        source  = "hashicorp/random"
-        version = "3.7.1"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.23.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+  }
 }
 
 data "azurerm_resource_group" "Rafael" {
-    name = "Rafael"
-    
+  name = "Rafael"
+
 }
 
 module "dns_zone_module" {
-    source = "./mod-dns"
-    #Declarar valores para serem usados no módulo (essas variaveis estao no arquivo mod-dns/variables.tf)
-    dns_name = "mello.com"
-    rg_name = data.azurerm_resource_group.Rafael.name
+  source = "./mod-dns"
+  #Declarar valores para serem usados no módulo (essas variaveis estao no arquivo mod-dns/variables.tf)
+  dns_name = "mello.com"
+  rg_name  = data.azurerm_resource_group.Rafael.name
 }
 
 #Output DNS Zone ID
 #Este output esta declarado no arquivo mod-dns/output.tf
 output "dns_zone_id" {
-    value = module.dns_zone_module.dns_zone_id
+  value = module.dns_zone_module.dns_zone_id
 }
