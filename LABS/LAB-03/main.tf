0303af8b2f6a1042d9ad19868576731efbd0a048
rafael Mello <rmellonh@gmail.com>
2025-03-24 21:49:10 -0400

    Terraform

---
+++ LABS/LAB-03/main.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "4.23.0"
+        }
+    
+        random = {
+        source  = "hashicorp/random"
+        version = "3.7.1"
+        }
+    }
+}
